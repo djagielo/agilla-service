4e4ed8f341dd7f8db0013720f1f2ee6b75dc4161
Dariusz Jagieło <dariusz.jagielo@gmail.com>
2019-06-18 21:49:05 +0200

    init commit for pet project

---
+++ src/main/java/me/djagielo/agillaservice/AgillaServiceApplication.java
@@ -0,0 +1,13 @@
+package me.djagielo.agillaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgillaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgillaServiceApplication.class, args);
+	}
+
+}
