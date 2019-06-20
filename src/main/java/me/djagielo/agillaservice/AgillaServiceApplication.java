b3ae0b694bf0276d259569c17aeddfb89b42a4ea
Dariusz Jagieło <dariusz.jagielo@gmail.com>
2019-06-20 15:23:53 +0200

    ping-pong endpoint for testing

--- src/main/java/me/djagielo/agillaservice/AgillaServiceApplication.java
+++ src/main/java/me/djagielo/agillaservice/AgillaServiceApplication.java
@@ -2,6 +2,9 @@ package me.djagielo.agillaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AgillaServiceApplication {
@@ -10,4 +13,11 @@ public class AgillaServiceApplication {
 		SpringApplication.run(AgillaServiceApplication.class, args);
 	}
 
+	@RestController
+	class TestController {
+		@GetMapping("/ping")
+		public String ping() {
+			return "pong";
+		}
+	}
 }
